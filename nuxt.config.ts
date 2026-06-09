eef22fb82bcdfb6496398c03fef3dc07777b58c4
Muadeel56 <muadeel59@gmail.com>
2026-06-09 15:09:09 +0500

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,42 @@
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  vite: {
+    plugins: [tailwindcss() as any],
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  modules: [
+    '@nuxt/image',
+    '@nuxtjs/google-fonts',
+    '@vueuse/nuxt',
+    '@nuxt/icon',
+    '@nuxt/content',
+  ],
+
+  googleFonts: {
+    families: {
+      Syne: [400, 600, 700, 800],
+      'DM Sans': [300, 400, 500, 600, 700],
+    },
+    display: 'swap',
+  },
+
+  app: {
+    head: {
+      title: 'Location Technologies Pvt. Ltd.',
+      meta: [
+        { name: 'description', content: 'GIS, GPS Tracking & Geofencing Solutions' },
+        { name: 'theme-color', content: '#0A1628' },
+      ],
+    },
+  },
+
+  nitro: {
+    prerender: { crawlLinks: true },
+  },
+})
